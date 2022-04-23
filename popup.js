8f2193593efdeab6ba93ec7b8b2fa6b0d6b0db23
一个小阚阚 <75866787+littlekan-gitee@users.noreply.github.com>
2022-04-23 19:10:56 +0800

    添加提示+列表表格式展示+更改部分js代码

--- popup.js
+++ popup.js
@@ -1,14 +1,32 @@
 const bg = chrome.extension.getBackgroundPage();
 let currentBlackList = bg.getBlackList();
 
-const blackListDom = document.getElementById("blackList");
+//const blackListDom = document.getElementById("blackList");
+const tip = document.getElementById("errtip");
+const blackTable = document.getElementById("banlist");
+
+function updateTable(){
+    const tableEl = ["<tr><td>","</td></tr>"];
+    var i = 0;
+    while(currentBlackList[i]){
+        blackTable.innerHTML = blackTable.innerHTML + tableEl[0] + currentBlackList[i] + tableEl[1];
+    }
+}
 
 document.getElementById('confirm').addEventListener("click", () => {
     const text = document.getElementById('name').value;
     if (text) {
         bg.addBlackList(text);
-        blackListDom.innerHTML = `<p>${JSON.stringify(currentBlackList)}</p>`;
+        tip.style.color = "green";
+        tip.innerHTML = "Info: 添加成功！";
+        updateTable();
+        //blackListDom.innerHTML = `<p>${JSON.stringify(currentBlackList)}</p>`;
+    }else{
+        tip.style.color = "red";
+        tip.innerHTML = "Error: 请输入昵称！";
     }
+    setTimeout(()=>{tip.innerHTML = "";},3000);
 })
 
-blackListDom.innerHTML = `<p>${JSON.stringify(currentBlackList)}</p>`;
+updateTable();
+//blackListDom.innerHTML = `<p>${JSON.stringify(currentBlackList)}</p>`;
